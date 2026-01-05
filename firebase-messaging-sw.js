631521c5d9808a336d68d87f263a388443ebcce1
ilyes <ezdiniilyes55@gmail.com>
2026-01-05 20:41:00 +0100

    Initial commit - FCM token generator

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB3k5sFmPSxzPdRNRweG_-GRFnEawSYySQ",
+  authDomain: "kifek-kifna.firebaseapp.com",
+  projectId: "kifek-kifna",
+  storageBucket: "kifek-kifna.firebasestorage.app",
+  messagingSenderId: "1053021314855",
+  appId: "1:1053021314855:web:ac007d5d1a17cf866f1302"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Background message:', payload);
+
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/icon.png'
+  });
+});
